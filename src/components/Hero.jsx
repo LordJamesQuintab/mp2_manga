cb7af74c90fae776a769f01a188c24c75d5e4471
unknown <jamjaminquintab@gmail.com>
2024-08-28 22:27:59 +0800

    updating

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div>
       <div className='flex flex-col items-center p-10 py-20 gap-6 h-[840px] w-full bg-[#d49ef2]'>
-        <h2 className='text-lg'>Find your Anime</h2>
+        <h2 className='text-lg'>Find your Anime Mangga</h2>
         <h2 className='text-[60px] font-bold'>Manga Hinog</h2>
 
         <Search/>
